14fa18343d02316b97ce51e6bfdfc4cec6b8a2e3
Matheus Feo <matheuspcsantos@gmail.com>
2018-12-23 20:27:04 -0200

    REFACTOR: Alterações código pós revisão
    Remoção dos comentários TODO conforme solicitado para requisito;
    Alteração estrutura do teste 'Menu elemento esta oculto';
    Reformulação teste 'existe um elemento em .feed' de acordo com a dica
    dada pelo revisor.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,25 +1,11 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
+    /* RSS Feeds - primeira suíte de testes.
+      São testados tudo relacionado ao array que contem os feeds.
     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+        /* 'are defined' - Teste escrito pela equipe Udacity como exemplo para
+        todo o projeto.
+            Nesse teste verifica-se se o array foi definido e se não está vazio
          */
         it('are defined', function() {
           expect(allFeeds).toBeDefined();
@@ -27,9 +13,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* Teste no qual é verificado se existe uma URL válida.
          */
         it('existe uma URL', function() {
           // Cria-se uma variável armazenando uma String para comparação com a URL de allFeeds
@@ -40,9 +24,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* Teste no qual é verificado se o nome foi definido.
          */
         it('existe um nome', function () {
           // Garante-se que o nome não é vazio e não seja um número
@@ -54,25 +36,21 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* O menu - segunda suíte de testes*/
     describe('O Menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* Teste onde é verificado se o menu está oculto por padrão.
+           Sendo verificado se o elemento body possui a classe que torna
+           o menu oculto.
          */
         it('Menu elemento esta oculto', function() {
-          // Garante-se que o elemento está oculto tendo a classe menu.hidden
-          expect(document.querySelector('.menu-hidden')).toBeTruthy();
+          expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
+         /* Teste onde é engatilhado o evento de click para se testar a
+            visibilidade do menu com o evento. Com o click inicial, o menu deverá
+            ser visualizado. Já no segundo clique, o mesmo deve ficar oculto.
           */
-
         it('visibilidade do menu', function() {
           // Engatilha-se o click
           $('.menu-icon-link').click();
@@ -84,13 +62,11 @@ $(function() {
           expect($('body').hasClass('menu-hidden')).toBe(true);
         });
     });
-    /* TODO: Write a new test suite named "Initial Entries" */
 
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
+    /* Entradas Iniciais - terceira suíte de testes*/
+
+    /* Suíte onde é testado e validado se existe ao menos 1 elemento de feeds
+       após chamar a função loadFeed().
      */
 
     describe('Entradas Inicias', function() {
@@ -100,18 +76,15 @@ $(function() {
         });
 
         it('existe um elemento em .feed', function(done) {
-          var entrada = $('article');
-          // Testa-se se o artigo possui a classe entry, indicando assim que o conteúdo foi carregado
-          expect(entrada.hasClass('entry')).toBeTruthy();
+          var entrada = $('.feed .entry');
+          expect(entrada).toBeTruthy();
           done();
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Nova seleção de feed - quarta suíte de testes */
 
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
+    /* Suíte onde se testa se o conteúdo do feed não está se duplicando.
      */
     describe('Nova seleção de feed', function() {
       var contAntigo, contNovo;
